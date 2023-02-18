e403cc96a01834177a13d4d25d3ee3780d8d19d0
nicoarias47 <ariasn1995@gmail.com>
2023-02-18 19:10:54 -0300

    view: products and finished circles mobile

--- src/db/db-fake.js
+++ src/db/db-fake.js
@@ -1,10 +1,11 @@
 import velas from "../assets/products/velas.jpg";
 import difusores from "../assets/products/difusores.jpg";
+
 export const db = {
   aromatizantes: [
     {
       id: 1,
-      title: "Velas",
+      title: "Aromatizantes",
       list: [
         "Deco de madera",
         "Centros de mesa",
@@ -20,129 +21,87 @@ export const db = {
   ],
   baño: [
     {
-      id: 1,
-      title: "Velas",
-      img: velas,
-      description:
-        "Velas para cada espacio de tu hogar con deliciosos aromas variados. Velas para cada espacio de taromasaromas",
-    },
-    {
       id: 2,
-      title: "Difusores",
-      img: difusores,
-      description:
-        "Velas para cada espacio de tu hogar con deliciosos aromas variados. Velas para cada espacio de taromasaromas",
-    },
-    {
-      id: 3,
-      title: "Difusores",
+      title: "Aromatizantes",
+      list: [
+        "Deco de madera",
+        "Centros de mesa",
+        "Cuadros",
+        "Ceramica",
+        "Relojes",
+        "Bandejas",
+        "Lámparas",
+        "Espejos",
+      ],
       img: velas,
-      description:
-        "Velas para cada espacio de tu hogar con deliciosos aromas variados. Velas para cada espacio de taromasaromas",
     },
   ],
   textil: [
     {
-      id: 1,
-      title: "Velas",
-      img: velas,
-      description:
-        "Velas para cada espacio de tu hogar con deliciosos aromas variados. Velas para cada espacio de taromasaromas",
-    },
-    {
-      id: 2,
-      title: "Difusores",
-      img: difusores,
-      description:
-        "Velas para cada espacio de tu hogar con deliciosos aromas variados. Velas para cada espacio de taromasaromas",
-    },
-    {
       id: 3,
-      title: "Difusores",
+      title: "Aromatizantes",
+      list: [
+        "Deco de madera",
+        "Centros de mesa",
+        "Cuadros",
+        "Ceramica",
+        "Relojes",
+        "Bandejas",
+        "Lámparas",
+        "Espejos",
+      ],
       img: velas,
-      description:
-        "Velas para cada espacio de tu hogar con deliciosos aromas variados. Velas para cada espacio de taromasaromas",
     },
   ],
   hogar_y_deco: [
     {
-      id: 1,
-      title: "Velas",
+      id: 4,
+      title: "Living & Deco",
+      list: [
+        "Deco de madera",
+        "Centros de mesa",
+        "Cuadros",
+        "Ceramica",
+        "Relojes",
+        "Bandejas",
+        "Lámparas",
+        "Espejos",
+      ],
       img: velas,
-      description:
-        "Velas para cada espacio de tu hogar con deliciosos aromas variados. Velas para cada espacio de taromasaromas",
-    },
-    {
-      id: 2,
-      title: "Difusores",
-      img: difusores,
-      description:
-        "Velas para cada espacio de tu hogar con deliciosos aromas variados. Velas para cada espacio de taromasaromas",
-    },
-    {
-      id: 3,
-      title: "Difusores",
-      img: velas,
-      description:
-        "Velas para cada espacio de tu hogar con deliciosos aromas variados. Velas para cada espacio de taromasaromas",
     },
   ],
   jardin: [
     {
-      id: 1,
-      title: "Velas",
+      id: 5,
+      title: "Aromatizantes",
+      list: [
+        "Deco de madera",
+        "Centros de mesa",
+        "Cuadros",
+        "Ceramica",
+        "Relojes",
+        "Bandejas",
+        "Lámparas",
+        "Espejos",
+      ],
       img: velas,
-      description:
-        "Velas para cada espacio de tu hogar con deliciosos aromas variados. Velas para cada espacio de taromasaromas",
-    },
-    {
-      id: 2,
-      title: "Difusores",
-      img: difusores,
-      description:
-        "Velas para cada espacio de tu hogar con deliciosos aromas variados. Velas para cada espacio de taromasaromas",
-    },
-    {
-      id: 3,
-      title: "Difusores",
-      img: velas,
-      description:
-        "Velas para cada espacio de tu hogar con deliciosos aromas variados. Velas para cada espacio de taromasaromas",
     },
   ],
   organizacion: [
     {
-      id: 1,
-      title: "Velas",
+      id: 6,
+      title: "Aromatizantes",
+      list: [
+        "Deco de madera",
+        "Centros de mesa",
+        "Cuadros",
+        "Ceramica",
+        "Relojes",
+        "Bandejas",
+        "Lámparas",
+        "Espejos",
+      ],
       img: velas,
-      description:
-        "Velas para cada espacio de tu hogar con deliciosos aromas variados. Velas para cada espacio de taromasaromas",
-    },
-    {
-      id: 2,
-      title: "Difusores",
-      img: difusores,
-      description:
-        "Velas para cada espacio de tu hogar con deliciosos aromas variados. Velas para cada espacio de taromasaromas",
-    },
-    {
-      id: 3,
-      title: "Difusores",
-      img: velas,
-      description:
-        "Velas para cada espacio de tu hogar con deliciosos aromas variados. Velas para cada espacio de taromasaromas",
     },
   ],
 };
-
-/* 
-
-{
-      id: 1,
-      title: "Velas",
-      list: ["Deco de madera", "Centros de mesa", "Cuadros","Ceramica", "Relojes", "Bandejas", "Lámparas","Espejos" ],
-      img: velas,
-},
-
-
-*/
--- src/views/Products.jsx
+++ src/views/Products.jsx
@@ -3,26 +3,41 @@ import { useState } from "react";
 import { Container } from "react-bootstrap";
 import { useParams } from "react-router-dom";
 import CarouselAbstract from "../components/CardWithCarousel/CardWithCarousel";
+import Suscribe from "../components/Suscribe/Suscribe";
 import { db } from "../db/db-fake";
 
 const Products = () => {
   const [itemSelect, setItemSelect] = useState([]);
+  const [loading, setLoading] = useState(true);
   const { producto } = useParams();
 
   useEffect(() => {
     setItemSelect(db[producto]);
+    setLoading(false);
   }, [producto]);
 
   return (
-    <Container expand="lg">
-      <section className="products py-5">
-        <div className="row">
-          <div className="col products_list">
-            <CarouselAbstract data={itemSelect} />
+    <>
+      <Container expand="lg">
+        <section className="products py-5">
+          <div className="row">
+            {loading ? (
+              ""
+            ) : (
+              <div className="col products_list">
+                <h1>{itemSelect[0].title}</h1>
+                <CarouselAbstract data={itemSelect} />
+              </div>
+            )}
           </div>
-        </div>
+        </section>
+        <Suscribe />
+      </Container>
+      <section className="products_section-footer">
+        <div className="circle"></div>
+        <div className="img"></div>
       </section>
-    </Container>
+    </>
   );
 };
 
