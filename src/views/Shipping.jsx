d5d7c60410a0b25c783ff3384753168163f5a362
nicoarias47 <ariasn1995@gmail.com>
2023-02-25 15:36:12 -0300

    shipping view responsive

--- src/views/Shipping.jsx
+++ src/views/Shipping.jsx
@@ -1,71 +1,97 @@
 import React from "react";
 import Doubts from "../components/Doubts/Doubts";
 import { Container } from "react-bootstrap";
+import imgMp from "../assets/icon/mercadopago.svg";
+import imgEfectivo from "../assets/icon/efectivo.svg";
+import imgSantander from "../assets/icon/santander.svg";
+import imgTransferencia from "../assets/icon/transferencia.svg";
 
 const Shipping = () => {
   return (
     <>
-      <Container fluid="lg">
-        <div className="shipping-container">
-          <div className="row">
-            <div className="col-left col-xs-12 col-md-6 col-lg-6 p-3">
-              <div className="background-left">
-                <div className="background-left-img"></div>
-                <div className="row d-flex justify-content-center background-left-description ">
-                  <h5 className="col-8 background-left-title ">Envios</h5>
+      <Container fluid="lg" className="shipping_section">
+        <div className="row shipping_section-grid">
+          <section className="col-12 shipping_section-hero">
+            <div className="shipping_section-img"></div>
+            <div className="row d-flex justify-content-center shipping_section-description ">
+              <h5 className="col-8 shipping_section-title ">Envios</h5>
+            </div>
+          </section>
+          <section className="col-12 shipping_section-content ">
+            <h6 className="shipping_content-title">
+              Realizamos envíos a nivel nacional por medio de Correo Argentino
+            </h6>
+
+            <div className="row shipping_content">
+              <div className="col-12 py-4">
+                <div className="col-12 py-2 d-flex justify-content-between">
+                  <h6 className="shipping_content-subtitle">Mercado Pago</h6>
+                  <img src={imgMp} alt="" className="px-5" />
+                </div>
+                <div className="col-12">
+                  <span>
+                    Una vez elijas los productos que vayas a comprar, comunícate
+                    con nosotros por WhatsApp y te enviaremos el detalle con el
+                    monto y el link a Mercado Pago para poder completar tu
+                    compra.
+                  </span>
+                </div>
+              </div>
+
+              <div className="col-12  py-4">
+                <div className="col-12 py-2 d-flex justify-content-between">
+                  <h6 className="shipping_content-subtitle">Santander</h6>
+                  <img src={imgSantander} alt="" className="px-5" />
+                </div>
+                <div className="col-12">
+                  <span>
+                    Una vez elijas los productos que vayas a comprar, comunícate
+                    con nosotros por WhatsApp y te enviaremos el detalle con el
+                    monto y el link a Genet para poder completar tu compra.
+                    Además aprovecha los descuentos que te ofrecen por día al
+                    pagar con este medio.
+                  </span>
+                </div>
+              </div>
+
+              <div className="col-12  py-4">
+                <div className="col-12 py-2 d-flex justify-content-between">
+                  <h6 className="shipping_content-subtitle">Efectivo</h6>
+                  <img src={imgEfectivo} alt="" className="px-5" />
+                </div>
+                <div className="col-12">
+                  <span>
+                    Podes pagar con efectivo al momento de retirar tu producto
+                    con previa coordinación.
+                  </span>
+                </div>
+              </div>
+
+              <div className="col-12  py-4">
+                <div className="col-12 py-2 d-flex justify-content-between">
+                  <h6 className="shipping_content-subtitle">Transferencia</h6>
+                  <img src={imgTransferencia} alt="" className="px-5" />
+                </div>
+                <div className="col-12">
+                  <span>
+                    Una vez elijas los productos que vayas a comprar, comunícate
+                    con nosotros por WhatsApp y te enviaremos el detalle con el
+                    monto y los datos necesarios para que puedas completar tu
+                    compra.
+                  </span>
                 </div>
               </div>
             </div>
-            <div className="col-right col-xs-12 col-md-6 col-lg-6 p-3">
-              <p className="body2">
-                Realizamos envíos a nivel nacional por medio de Correo Argentino
-              </p>
-
-              <div className="sub-col-container">
-                <div className="sub-col-header col-12">
-                  <p className="body2-title">
-                    ¿Cuánto tarda en llegar mi pedido?{" "}
-                  </p>
-                </div>
-                <p className="col-12 body3">
-                  Nos toma entre 48 y 72 horas en despachar tu pedido a la
-                  empresa de correo.
-                  <br />
-                  El tiempo de llegada, depende de la compañía de correo y de tu
-                  ubicación.
-                </p>
+          </section>
+          <div className="col-12 py-5 shipping_section-doubts">
+            <div className="row">
+              <div className="col"></div>
+              <div className="col-sm-12 col-md-12">
+                <Doubts />
               </div>
-              <div className="sub-col-container">
-                <div className="sub-col-header col-12">
-                  <p className="body2-title"> Seguimiento de los pedidos </p>
-                </div>
-                <p className="body3">
-                  Al momento en el que tu pedido este en manos de la empresa de
-                  envíos se te entregara un numero de guía para que puedas
-                  hacerle seguimiento a tu pedido y puedas estar pendiente de su
-                  llegada.
-                </p>
-              </div>
-              <div className="sub-col-container">
-                <div className="sub-col-header col-12">
-                  <p className="body2-title"> Envíos exprés </p>
-                </div>
-                <p className="body3">
-                  No realizamos pedidos expres ya que dependemos de los tiempos
-                  de entrega de las compañía.
-                </p>
-              </div>
-              <div className="sub-col-container">
-                <p className="body3">
-                  Si deseas saber más sobre los costos de despacho clickea en el
-                  siguiente enlace.
-                </p>
-                <br />
-                <a href="#">Ver costos de despacho</a>
-              </div>
+              <div className="col"></div>
             </div>
           </div>
-          <Doubts />
         </div>
       </Container>
       <section className="shipping-footer">
